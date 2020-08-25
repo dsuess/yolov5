a68162c37fbe624a8f2ce63bf4e16d5c8c4de06e
Daniel Suess <daniel@dsuess.me>
2020-08-25 14:41:42 +1000

    Make ONNX export work

--- models/export.py
+++ models/export.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     attempt_download(opt.weights)
     model = torch.load(opt.weights, map_location=torch.device('cpu'))['model'].float()
     model.eval()
-    model.model[-1].export = True  # set Detect() layer export=True
+    model.model[-1].export = False  # set Detect() layer export=True
     y = model(img)  # dry run
 
     # TorchScript export
